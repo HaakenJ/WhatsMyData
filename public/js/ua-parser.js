3804c8727523ce40e531d8f08cad354958b0fc49
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-10 18:45:28 -0800

    Added button links to more info about hardware and software.

--- public/js/ua-parser.js
+++ public/js/ua-parser.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
-    const parser = new UAParser();
-    const device = parser.getDevice();
-    const cpu = parser.getCPU();
-    const os = parser.getOS();
+    const parser = new UAParser(),
+        device = parser.getDevice(),
+        cpu = parser.getCPU(),
+        os = parser.getOS(),
+        browser = parser.getBrowser();
 
     if (device.type === undefined) {
         $("#device-type").text("Desktop PC");
@@ -13,12 +14,24 @@ $(document).ready(() => {
         $("#device-vendor").text(device.vendor);
         $("#device-model").text(device.model);
     }
+
     $("#cpu").text(cpu.architecture);
     $("#cores").text(navigator.hardwareConcurrency);
+
     navigator.getBattery().then((battery) => {
         $("#battery-charging").text(battery.charging ? "Yes" : "No");
         $("#battery-level").text(`${battery.level * 100}%`);
     });
 
     $("#op-sys").text(`${os.name} ${os.version}`);
+    $("#browser").text(`${browser.name} ${browser.version}`)
+
+    const pluginArray = navigator.plugins;
+
+    for (let i = 0; i < pluginArray.length; i++) {
+        let currentPlugin = $("<li>")
+            .text(pluginArray[i].name)
+            .addClass("plugin-name");
+        $("#plugins").append(currentPlugin);
+    }
 });
