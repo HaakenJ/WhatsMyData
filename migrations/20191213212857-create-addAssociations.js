e59ebd449edf34f82019d571fb3fd65e2d527980
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-13 15:51:50 -0800

    Got all tables set up with foreign keys.

---
+++ migrations/20191213212857-create-addAssociations.js
@@ -0,0 +1,169 @@
+'use strict';
+module.exports = {
+  // Add the foreign keys when db:migrate is called
+  up: (queryInterface, Sequelize) => {
+    // Add IDid to Hardware table
+    return queryInterface.addColumn(
+        "Hardware", // Name of the source model (to add key to)
+        "IPid", // Name of the key we are adding
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: "IP", // Target model
+            key: "id" // Key in the target model we are referencing
+          },
+          onUpdate: "CASCADE",
+          onDelete: "SET NULL"
+        },
+      )
+      // We can call .then() and chain these queryInterface methods because
+      // they return promises.
+      .then(() => {
+        // Add IDid to Software table
+        return queryInterface.addColumn(
+            "Software",
+            "IPid", {
+              type: Sequelize.INTEGER,
+              references: {
+                model: "IP",
+                key: "id"
+              },
+              onUpdate: "CASCADE",
+              onDelete: "SET NULL"
+            }
+          )
+          .then(() => {
+            return queryInterface.addColumn(
+                "Locations",
+                "IPid", {
+                  type: Sequelize.INTEGER,
+                  references: {
+                    model: "IP",
+                    key: "id"
+                  },
+                  onUpdate: "CASCADE",
+                  onDelete: "SET NULL"
+                }
+              )
+              .then(() => {
+                return queryInterface.addColumn(
+                    "Logins",
+                    "IPid", {
+                      type: Sequelize.INTEGER,
+                      references: {
+                        model: "IP",
+                        key: "id"
+                      },
+                      onUpdate: "CASCADE",
+                      onDelete: "SET NULL"
+                    }
+                  )
+                  .then(() => {
+                    return queryInterface.addColumn(
+                        "IP",
+                        "HardwareId", {
+                          type: Sequelize.INTEGER,
+                          references: {
+                            model: "Hardware",
+                            key: "id"
+                          },
+                          onUpdate: "CASCADE",
+                          onDelete: "SET NULL"
+                        }
+                      )
+                      .then(() => {
+                        return queryInterface.addColumn(
+                            "IP",
+                            "SoftwareId", {
+                              type: Sequelize.INTEGER,
+                              references: {
+                                model: "Software",
+                                key: "id"
+                              },
+                              onUpdate: "CASCADE",
+                              onDelete: "SET NULL"
+                            }
+                          )
+                          .then(() => {
+                            return queryInterface.addColumn(
+                                "IP",
+                                "LocationsId", {
+                                  type: Sequelize.INTEGER,
+                                  references: {
+                                    model: "Locations",
+                                    key: "id"
+                                  },
+                                  onUpdate: "CASCADE",
+                                  onDelete: "SET NULL"
+                                }
+                              )
+                              .then(() => {
+                                return queryInterface.addColumn(
+                                  "IP",
+                                  "LoginsId", {
+                                    type: Sequelize.INTEGER,
+                                    references: {
+                                      model: "Logins",
+                                      key: "id"
+                                    },
+                                    onUpdate: "CASCADE",
+                                    onDelete: "SET NULL"
+                                  }
+                                )
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  },
+  down: (queryInterface, Sequelize) => {
+    // Remove the new columns when db:migrate:undo is called
+    return queryInterface.removeColumn(
+        "Hardware", // Name of source model
+        "IPid" // Key we want to remove
+      )
+      .then(() => {
+        return queryInterface.removeColumn(
+            "Software",
+            "IPid"
+          )
+          .then(() => {
+            return queryInterface.removeColumn(
+                "Locations",
+                "IPid"
+              )
+              .then(() => {
+                return queryInterface.removeColumn(
+                  "Logins",
+                  "IPid"
+                ).then(() => {
+                  return queryInterface.removeColumn(
+                    "IP",
+                    "HardwareId"
+                  )
+                  .then(() => {
+                    return queryInterface.removeColumn(
+                      "IP",
+                      "SoftwareId"
+                    )
+                    .then(() => {
+                      return queryInterface.removeColumn(
+                        "IP",
+                        "LocationsId"
+                      )
+                      .then(() => {
+                        return queryInterface.removeColumn(
+                          "IP",
+                          "LoginsId"
+                        );
+                      });
+                    });
+                  });
+                });
+              });
+          });
+      });
+  }
+};
---
+++ models/addAssociations.js
@@ -0,0 +1,10 @@
+// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   const addAssociations = sequelize.define('addAssociations', {
+//     IPid: DataTypes.INTEGER
+//   }, {});
+//   add - associations.associate = function(models) {
+//     // associations can be defined here
+//   };
+//   return add - associations;
+// };
