7aa96277faab35a427207e4edd4fa9323c014731
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-13 16:20:35 -0800

    Created variables in requests for all the data to be gathered from the page.

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,3 +1,19 @@
 $(document).ready(() => {
-    
+  let devType,
+    vendor,
+    model,
+    cpu,
+    cores,
+    batteryLevel,
+    ipAdd,
+    continent,
+    country,
+    city,
+    latitude,
+    longitude,
+    logins,
+    os,
+    osVersion,
+    browser,
+    browserVersion;  
 })
