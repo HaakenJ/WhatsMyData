50a7e38e9dbd0bc3e4f6a761a98d58c649835ac4
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-14 16:47:40 -0800

    Tried to run npm browser history with no luck, removed commented out server code.

--- server.js
+++ server.js
@@ -27,13 +27,6 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// app.listen(PORT, () => {
-//   console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//     PORT,
-//     PORT
-//   );
-// })
-
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
