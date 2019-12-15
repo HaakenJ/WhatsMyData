ecadfebeef0e91e5868af549aee6ee8af000eb64
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-14 17:12:57 -0800

    Added  pie chart to index.html with dummy data

---
+++ public/js/charts.js
@@ -0,0 +1,35 @@
+// Load the Visualization API and the corechart package.
+google.charts.load("current", {
+  packages: ["corechart"]
+});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+  // Create the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn("string", "Device-Type");
+  data.addColumn("number", "NumberOfUsers");
+  data.addRows([
+    ["PC", 43],
+    ["Mobile", 65],
+    ["Tablet", 23]
+  ]);
+
+  // Set chart options
+  var options = {
+    title: "What Kinds of Devices Our Users Visit With",
+    width: 500,
+    height: 400
+  };
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.PieChart(
+    document.getElementById("chart_div")
+  );
+  chart.draw(data, options);
+}
