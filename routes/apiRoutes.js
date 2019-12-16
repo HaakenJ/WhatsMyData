57bd577b1aefe37372270b206931469c6e7aeddc
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-16 14:03:58 -0800

    Removed unecessary code from api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,6 @@
 const db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
-
-  // Create a new example
   app.post("/api/userData", function(req, res) {
     console.log(req.body);
     db.userdata.create(req.body).then(dbuserdata => {
@@ -33,11 +25,4 @@ module.exports = function(app) {
       res.json(browserInfo);
     });
   });
-
-  // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
 };
