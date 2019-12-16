68068850602bb12ebac308749faea43c2396b280
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-16 12:01:23 -0800

    Chart on webpage now updates with data from database.

--- public/js/charts.js
+++ public/js/charts.js
@@ -3,37 +3,40 @@ google.charts.load("current", {
   packages: ["corechart"]
 });
 
-$.get("/api/devtype", (data) => {
-  console.log(data);
-});
+$.get("/api/devtype", (response) => {
+  let chartData = [];
+  console.log(response);
+  response.forEach((obj) => {
+    let deviceData = [obj.devType, obj.no_devType];
+    chartData.push(deviceData);
+  });
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
-  // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn("string", "Device-Type");
-  data.addColumn("number", "NumberOfUsers");
-  data.addRows([
-    ["PC", 43],
-    ["Mobile", 65],
-    ["Tablet", 23]
-  ]);
+  console.log(chartData);
 
   // Set a callback to run when the Google Visualization API is loaded.
   google.charts.setOnLoadCallback(drawChart);
 
-  // Set chart options
-  var options = {
-    title: "What Kinds of Devices Our Users Visit With",
-    width: 500,
-    height: 400
-  };
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn("string", "Device-Type");
+    data.addColumn("number", "NumberOfUsers");
+    data.addRows(chartData);
 
-  // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(data, options);
-}
+    // Set chart options
+    var options = {
+      title: "What Kinds of Devices Our Users Visit With",
+      width: 500,
+      height: 400
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(
+      document.getElementById("chart_div")
+    );
+    chart.draw(data, options);
+  }
+});
