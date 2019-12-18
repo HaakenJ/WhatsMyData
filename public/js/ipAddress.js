673cf5cd747d0a4d30afeb68e1f98c207aed88b0
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-18 09:08:17 -0800

    Updated ipAddresses file to match Michael's

--- public/js/ipAddress.js
+++ public/js/ipAddress.js
@@ -1,44 +1,30 @@
-// $(document).ready(() => {
-//     var ip = sessionStorage.getItem("ip");
-//     var country_name = sessionStorage.getItem("country_name");
-//     var country_code2 = sessionStorage.getItem("country_code2");
+var ip = sessionStorage.getItem("ip");
+var country_name = sessionStorage.getItem("country_name");
+var city = sessionStorage.getItem("ccity");
 
-//     if (!ip || !country_name || !country_code2) {
-//         setAsync(false);
-//         setFieldsParameter("country_name,country_code2");
-//         getGeolocation(handleGeolocationResponse, "93434439bff2436bba87545501a9a061");
-//     }
+if (!ip || !country_name || !city) {
+    setAsync(false);
+    setFieldsParameter("country_name,city");
+    getGeolocation(handleGeolocationResponse, "93434439bff2436bba87545501a9a061");
+}
 
-//     function handleGeolocationResponse(json) {
-//         ip = json.ip;
-//         country_name = json.country_name;
-//         country_code2 = json.country_code2;
+function handleGeolocationResponse(json) {
+    ip = json.ip;
+    country_name = json.country_name;
+    city = json.city;
 
-//         sessionStorage.setItem("ip", ip);
-//         sessionStorage.setItem("country_name", country_name);
-//         sessionStorage.setItem("country_code2", country_code2);
+    sessionStorage.setItem("ip", ip);
+    sessionStorage.setItem("country_name", country_name);
+    sessionStorage.setItem("city", city);
+}
 
-//         console.log(ip, country_name, country_code2);
-//     }
 
-//     // console.log(ip);
-//     // $("#ip-address").text(ip);
-// })
+console.log(country_name);
+console.log(city);
+console.log(ip);
+$(document).ready(function () {
+    $("#ip-address").text(ip);
+    $("#city").text(city);
+    $("#country").text(country_name);
 
-$(document).ready(() => {
-    $.ajax({
-        type: "GET",
-        url: "https://api.ipify.org?format=json"
-    }).then((ipResponse) => {
-        $("#ip-address").text(ipResponse.ip);
-        let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=${ipResponse.ip}&fields=country_code2,country_name,city`;
-        $.ajax({
-            type: "GET",
-            url: url
-        }).then(locationInfo => {
-            $("#city").text(locationInfo.city);
-            $("#country").text(locationInfo.country_name);
-            $("#country-code").text(locationInfo.country_code2);
-        });
-    });
 });
