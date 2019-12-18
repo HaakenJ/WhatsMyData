f602344e66a12c129f010be0c900de379c655506
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-17 16:28:19 -0800

    Fixed column chart to receive the proper data.

--- public/js/charts.js
+++ public/js/charts.js
@@ -48,52 +48,58 @@ $.get("/api/browser", (response) => {
         'Chrome WebView', 'Edge', 'Internet Explorer', 'Opera', 'Other'
       ]
     ],
-    objOfArrays = {};
+    recordObject = {};
 
-  // Create an array in the proper format for each device type and 
-  // add it to obj of arrays.
-  response.forEach(record => {
-    // If the device type hasn't been added to obj, then add 
-    // it with blank values for all columns.
-    if (!objOfArrays[record.devType]) {
-      objOfArrays[record.devType] = [record.devType, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    }
 
-    // Depending on what browser is in the record, add that value
-    // to the proper location in the array. 
-    switch (record.browser) {
-      case "Chrome":
-        objOfArrays[record.devType][1] = record.no_browser;
-        break;
-      case "Mobile Safari":
-        objOfArrays[record.devType][2] = record.no_browser;
-        break;
-      case "Firefox":
-        objOfArrays[record.devType][3] = record.no_browser;
-        break;
-      case "Silk":
-        objOfArrays[record.devType][4] = record.no_browser;
-        break;
-      case "Chrome WebView":
-        objOfArrays[record.devType][5] = record.no_browser;
-        break;
-      case "Edge":
-        objOfArrays[record.devType][6] = record.no_browser;
-        break;
-      case "Internet Explorer":
-        objOfArrays[record.devType][7] = record.no_browser;
-        break;
-      case "Opera":
-        objOfArrays[record.devType][8] = record.no_browser;
-        break;
-      default:
-        objOfArrays[record.devType][9] = record.no_browser;
-    }
-  });
+  // Loop through the three arrays contained in the reponse object.
+  // Three represent the browsers used by PCs, tablets, and mobile phones.
+  for (let i = 0; i < Object.keys(response).length; i++) {
+    // Create an array in the proper format for each device type and 
+    // add it to obj of arrays.
+    response[Object.keys(response)[i]].forEach(record => {
+      // If the device type hasn't been added to obj, then add 
+      // it with blank values for all columns.
+      if (!recordObject[record.devType]) {
+        recordObject[record.devType] = [record.devType, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      }
+
+      // Depending on what browser is in the record, add that value
+      // to the proper location in the array. 
+      switch (record.browser) {
+        case "Chrome":
+          recordObject[record.devType][1] = record.no_browser;
+          break;
+        case "Mobile Safari":
+          recordObject[record.devType][2] = record.no_browser;
+          break;
+        case "Firefox":
+          recordObject[record.devType][3] = record.no_browser;
+          break;
+        case "Silk":
+          recordObject[record.devType][4] = record.no_browser;
+          break;
+        case "Chrome WebView":
+          recordObject[record.devType][5] = record.no_browser;
+          break;
+        case "Edge":
+          recordObject[record.devType][6] = record.no_browser;
+          break;
+        case "Internet Explorer":
+          recordObject[record.devType][7] = record.no_browser;
+          break;
+        case "Opera":
+          recordObject[record.devType][8] = record.no_browser;
+          break;
+        default:
+          recordObject[record.devType][9] = record.no_browser;
+      }
+    });
+  }
+
 
   // Add the arrays of data to the chartData array.
-  for (let i = 0; i < Object.keys(objOfArrays).length; i++) {
-    chartData[i + 1] = objOfArrays[Object.keys(objOfArrays)[i]];
+  for (let i = 0; i < Object.keys(recordObject).length; i++) {
+    chartData[i + 1] = recordObject[Object.keys(recordObject)[i]];
   };
 
   google.charts.setOnLoadCallback(drawChart);
@@ -127,7 +133,9 @@ $.get("/api/location", (countryData) => {
 
   google.charts.setOnLoadCallback(drawRegionsMap);
 
-  let chartData = [["Country", "Popularity"]];
+  let chartData = [
+    ["Country", "Popularity"]
+  ];
   countryData.forEach((record) => {
     let newRow = [record.country, record.no_country];
     chartData.push(newRow);
