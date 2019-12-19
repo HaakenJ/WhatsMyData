590e3977c673014a25e2d1a8a0844aacdf4d2585
Haakasaurus <kramerhjohnson@gmail.com>
2019-12-18 18:45:18 -0800

    Added latitude and longitude

--- public/js/ipAddress.js
+++ public/js/ipAddress.js
@@ -1,10 +1,12 @@
 var ip = sessionStorage.getItem("ip");
 var country_name = sessionStorage.getItem("country_name");
 var country_code2 = sessionStorage.getItem("country_code2");
+var latitude = sessionStorage.getItem("latitude");
+var longitude = sessionStorage.getItem("longitude");
 
-if (!ip || !country_name || !country_code2) {
+if (!ip || !country_name || !country_code2 || !latitude || !longitude) {
   setAsync(false);
-  setFieldsParameter("country_name,country_code2");
+  setFieldsParameter("country_name,country_code2,latitude,longitude");
   getGeolocation(handleGeolocationResponse, "93434439bff2436bba87545501a9a061");
 }
 
@@ -12,10 +14,14 @@ function handleGeolocationResponse(json) {
   ip = json.ip;
   country_name = json.country_name;
   country_code2 = json.country_code2;
+  latitude = json.latitude;
+  longitude = json.longitude;
 
   sessionStorage.setItem("ip", ip);
   sessionStorage.setItem("country_name", country_name);
   sessionStorage.setItem("country_code2", country_code2);
+  sessionStorage.setItem("latitude", latitude);
+  sessionStorage.setItem("longitude", longitude);
 }
 
 
@@ -26,4 +32,6 @@ $(document).ready(function () {
   $("#ip-address").text(ip);
   $("#country").text(country_name);
   $("#country-code").text(country_code2);
+  $("#latitude").text(latitude);
+  $("#longitude").text(longitude);
 });
